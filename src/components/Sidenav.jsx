f73fcbcb97889c0301561fb5fa48e401ba059fa3
geekqq <jinwei1014@gmail.com>
2023-05-17 22:06:40 -0700

    change sideNav: deleted onClick when displayed on the bigger screen

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -66,19 +66,19 @@ const Sidenav = () => {
                 )}
                 <div className="md:block hidden fixed top-[25%] z-10">
                     <div className="flex flex-col">
-                        <a onClick={handleNav} href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                        <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                             <AiOutlineHome size={20} />
                         </a>
-                        <a onClick={handleNav} href="#work" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                        <a href="#work" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                             <GrProjects size={20} />
                         </a>
-                        <a onClick={handleNav} href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                        <a href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                             <AiOutlineProject size={20} />
                         </a>
-                        <a onClick={handleNav} href="https://resume.iscuu.com" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                        <a href="https://resume.iscuu.com" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                             <BsPerson size={20} />
                         </a>
-                        <a onClick={handleNav} href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                        <a href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                             <AiOutlineMail size={20} />
                         </a>
                     </div>
